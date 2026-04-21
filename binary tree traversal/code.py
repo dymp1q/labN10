e08aeac57018d76b299c0cb57502ebaa57e57479
dymp1q <dmytrivmaryan2008@gmail.com>
2026-04-21 10:45:19 +0300

    Add funcs pre, in, post order
    Implemented pre-order, in-order, and post-order traversal functions for binary trees

---
+++ binary tree traversal/code.py
@@ -0,0 +1,23 @@
+class Node:
+  def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        
+# Pre-order traversal
+def pre_order(node):
+    if node is None:
+        return []
+    return [node.data] + pre_order(node.left) + pre_order(node.right)
+
+# In-order traversal
+def in_order(node):
+    if node is None:
+        return []
+    return in_order(node.left) + [node.data] + in_order(node.right)
+
+# Post-order traversal
+def post_order(node):
+    if node is None:
+        return []
+    return post_order(node.left) + post_order(node.right) + [node.data]
